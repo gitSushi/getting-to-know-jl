106f1202d394801b494b986d4edbd4ea3ad1a8fd
gitSushi <lossushi37@gmail.com>
2020-09-18 18:16:14 +0200

    Updates

---
+++ precache-manifest.88b6a4de59ea5b95a499635588e04e55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f7b5aa8af42138d3c75756ebc42373c",
+    "url": "/getting-to-know-jl/index.html"
+  },
+  {
+    "revision": "7d60cbc64b2a18a8b9d7",
+    "url": "/getting-to-know-jl/static/css/main.30982dd1.chunk.css"
+  },
+  {
+    "revision": "ab16bf51199ff647944b",
+    "url": "/getting-to-know-jl/static/js/2.578b4a7c.chunk.js"
+  },
+  {
+    "revision": "125be3a1dacd13f893e7386c7a3b07f6",
+    "url": "/getting-to-know-jl/static/js/2.578b4a7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d60cbc64b2a18a8b9d7",
+    "url": "/getting-to-know-jl/static/js/main.3014e2da.chunk.js"
+  },
+  {
+    "revision": "978fd84c9290b0482a09",
+    "url": "/getting-to-know-jl/static/js/runtime-main.f82ca7cf.js"
+  }
+]);
